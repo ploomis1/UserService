123299f020cb59a17aece68fd40b9db58d487113
parker.loomis1 <parker.loomis1@gmail.com>
2021-06-07 18:45:40 -0400

    initial push

---
+++ src/main/java/com/revature/dmv/users/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.dmv.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
